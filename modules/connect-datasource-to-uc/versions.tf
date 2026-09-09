6077104532cd9b4683f2e344a1337c938886372e
Jun Wang <wangwjun@outlook.com>
2026-09-09 17:12:56 -0400

    Update versions.tf

--- modules/connect-datasource-to-uc/versions.tf
+++ modules/connect-datasource-to-uc/versions.tf
@@ -6,8 +6,8 @@ terraform {
     guardium-data-protection = {
       # Repo name: terraform-provider-guardium-data-protection
       source  = "IBM/guardium-data-protection"
-      # Tag: v1.5.2
-      version = "= 1.5.2"
+      # Tag: v1.4.9
+      version = "= 1.4.9"
     }
   }
 }
